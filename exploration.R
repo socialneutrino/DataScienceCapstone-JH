fce99517a4006cd5444d4f6a7bb545e30b3d8d2b
socialneutrino <advspiers@gmail.com>
2015-12-25 22:36:08 +0000

    added cumulative freq data frames and plots

--- exploration.R
+++ exploration.R
@@ -1,108 +1,128 @@
-#Create unigram freq table
-total_sample <- c(blog_sample, twitter_sample, news_sample)
+library(ggplot2)
+library(quanteda)
+library(dplyr)
+
+source("tokenization.R")
+
+#Create unigram freq table for sample of ALL THREE corpora samples
+total_sample <- c(blogs_sample, twitter_sample, news_sample)
 unigrams <- makeTokens(total_sample)
 
 #Create bigram freq table
-a2 <- bigrams(total_sample)
-a2 <- dfm(a2)
-a2 <- colSums(sort(a2))
-bigrams_freq <- a2[grepl("[A-Za-z]+_[A-Za-z]+", names(a2))]
+bigrams_freq  <- bigrams(total_sample)
 
 #Create trigram freq table
-a3 <- trigrams(total_sample)
-a3 <- dfm(a3)
-a3 <- colSums(sort(a3))
-trigrams_freq <- a3[grepl("[A-Za-z]+_[A-Za-z]+_[A-Za-z]+", names(a3))]
+trigrams_freq <- trigrams(total_sample)
 
 
 
+#ngrams for BLOGS ONLY
+#unigrams
+blogs_unigrams <- makeTokens(blogs_sample)
+#Create bigram freq table
+blogs_bigrams <- bigrams(blogs_sample)
+#Create trigram freq table
+blogs_trigrams <- trigrams(blog_sample)
 
 
+#ngrams for TWITTER ONLY
+#unigrams
+twitter_unigrams <- makeTokens(twitter_sample)
+#Create bigram freq table
+twitter_bigrams <- bigrams(twitter_sample)
+#Create trigram freq table
+twitter_trigrams <- trigrams(twitter_sample)
 
 
+#ngrams for NEWS ONLY
+#unigrams
+news_unigrams <- makeTokens(news_sample)
+#Create bigram freq table
+news_bigrams <- bigrams(news_sample)
+#Create trigram freq table
+news_trigrams <- trigrams(news_sample)
+
+library(dplyr)
+
+#Create Cumulative Frequency Data Frame
+unigrams_freq <- as.data.frame(unigrams) %>% 
+    select(freq = 1) %>% 
+    mutate(unigram = names(unigrams))
+
+unigrams_freq$word_number <- 1:nrow(unigrams_freq)
+
+unigrams_freq <- unigrams_freq %>% 
+    mutate(cumsum = cumsum(freq)) %>%
+    mutate(coverage = 100*cumsum/sum(freq))
+
+#FUNCTION to Create Cumulative Frequency Data Frame
+freq_df <- function(table) {
+  a <- as.data.frame(table) %>% 
+    select(freq = 1) %>% 
+    mutate(table = names(table))
+  
+  a$word_number <- 1:nrow(a)
+  
+  a <- a %>% 
+    mutate(cumsum = cumsum(freq)) %>%
+    mutate(coverage = 100*cumsum/sum(freq))
+  
+  a
+}
+
+#TOTAL word and culumative frequency tables
+unigrams_freq <- freq_df(unigrams)
+bigrams_freq_df <- freq_df(bigrams_freq)
+trigrams_freq_df <- freq_df(trigrams_freq)
+
+#Plot of word number and cumulative percentage
+gg <- ggplot(unigrams_freq, aes(x=word_number, y=coverage))
+gg <- gg + geom_line(size=0.75)
+gg <- gg + scale_x_continuous(limits=c(1, nrow(unigrams_freq)))
+gg <- gg + theme_bw()
+gg
 
 
+#Blogs word coverage
+blogs_unigrams_freq <- freq_df(blogs_unigrams)
+
+gg <- ggplot(blogs_unigrams_freq, aes(x=word_number, y=coverage))
+gg <- gg + geom_line(size=0.75)
+gg <- gg + scale_x_continuous(limits=c(1, nrow(blogs_unigrams_freq)))
+gg <- gg + theme_bw()
+gg
 
 
+#News word coverage
+news_unigrams_freq <- freq_df(news_unigrams)
+
+gg <- ggplot(news_unigrams_freq, aes(x=word_number, y=coverage))
+gg <- gg + geom_line(size=0.75)
+gg <- gg + scale_x_continuous(limits=c(1, nrow(news_unigrams_freq)))
+gg <- gg + theme_bw()
+gg
 
 
+#Twitter word coverage
+twitter_unigrams_freq <- freq_df(twitter_unigrams)
 
+gg <- ggplot(twitter_unigrams_freq, aes(x=word_number, y=coverage))
+gg <- gg + geom_line(size=0.75)
+gg <- gg + scale_x_continuous(limits=c(1, nrow(twitter_unigrams_freq)))
+gg <- gg + theme_bw()
+gg
 
+#Bigram coverage
+gg <- ggplot(bigrams_freq_df, aes(x=word_number, y=coverage))
+gg <- gg + geom_line(size=0.75)
+gg <- gg + scale_x_continuous(limits=c(1, nrow(bigrams_freq_df)))
+gg <- gg + theme_bw()
+gg
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-library(openNLP)
-require("NLP")
-
-## Some text
-s <- paste(c("Pierre Vinken, 61 years old, will join the board as a ",
-             "nonexecutive director Nov. 29.\n",
-             "Mr. Vinken is chairman of Elsevier N.V., ",
-             "the Dutch publishing group."),
-           collapse = "")
-s <- as.String(s)
-
-
-## Need sentence and word token annotations.
-sent_token_annotator <- Maxent_Sent_Token_Annotator()
-word_token_annotator <- Maxent_Word_Token_Annotator()
-a2 <- annotate(s, list(sent_token_annotator, word_token_annotator))
-pos_tag_annotator <- Maxent_POS_Tag_Annotator()
-pos_tag_annotator
-a3 <- annotate(s, pos_tag_annotator, a2)
-a3
-
-
-## Variant with POS tag probabilities as (additional) features.
-head(annotate(s, Maxent_POS_Tag_Annotator(probs = TRUE), a2))
-
-
-## Determine the distribution of POS tags for word tokens.
-a3w <- subset(a3, type == "word")
-tags <- sapply(a3w$features, `[[`, "POS")
-tags
-table(tags)
-
-
-## Extract token/POS pairs (all of them): easy.
-sprintf("%s/%s", s[a3w], tags)
-
-## Extract pairs of word tokens and POS tags for second sentence:
-a3ws2 <- annotations_in_spans(subset(a3, type == "word"),
-                              subset(a3, type == "sentence")[2L])[[1L]]
-
-sprintf("%s/%s", s[a3ws2], sapply(a3ws2$features, `[[`, "POS"))
-
-library(tm)
-blog_corpus <- VCorpus(VectorSource(blog_sample))
+#Trigram coverage
+gg <- ggplot(trigrams_freq_df, aes(x=word_number, y=coverage))
+gg <- gg + geom_line(size=0.75)
+gg <- gg + scale_x_continuous(limits=c(1, nrow(trigrams_freq_df)))
+gg <- gg + theme_bw()
+gg
 
